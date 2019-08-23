31e0d0c653ec3607e1fe2f6fc6939c5be12391ea
Marc Rußwurm <marc.russwurm@tum.de>
2019-08-23 12:30:26 +0200

    initial commit

---
+++ transformer/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+import transformer.Constants
+import transformer.Modules
+import transformer.Layers
+import transformer.SubLayers
+import transformer.Models
+import transformer.Translator
+import transformer.Beam
+
+__all__ = [
+    transformer.Constants, transformer.Modules, transformer.Layers,
+    transformer.SubLayers, transformer.Models,
+    transformer.Translator, transformer.Beam]
